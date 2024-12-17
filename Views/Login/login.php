5a0f22e2afee3c96dc8364da468a9e05a2fea285
Ifhami <famishmn@gmail.com>
2024-12-17 15:15:21 +0800

    login

--- Views/Login/login.php
+++ Views/Login/login.php
@@ -20,36 +20,45 @@ include '../../public/includes/loginAuth.php';
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
 </head>
 <body>
-  <div class="wrapper">
-    <header>Login Form</header>
+<div>
+    <h1>RAPIDPRINT SYSTEM</h1>
+  </div>
+  <br>
 
-    <!-- Display error message if login fails -->
-    <?php if (!empty($error_message)): ?>
-      <div id="notification" style="padding: 10px; background-color: #dc3545; color: white; text-align: center;">
-        <?php echo $error_message; ?>
-      </div>
-    <?php endif; ?>
-
-    <form action="login.php" method="post">
-      <div class="field email">
-        <div class="input-area">
-          <input type="text" placeholder="Email Address" name="email" required>
-          <i class="icon fas fa-envelope"></i>
-          <i class="error error-icon fas fa-exclamation-circle"></i>
-        </div>
-      </div>
-      <div class="field password">
-        <div class="input-area">
-          <input type="password" placeholder="Password" name="password" required>
-          <i class="icon fas fa-lock"></i>
-          <i class="error error-icon fas fa-exclamation-circle"></i>
-        </div>
-      </div>
-      <div class="pass-txt"><a href="../ForgotPassword/forgot-password.php">Forgot password?</a></div>
-      <input type="submit" value="Login">
-    </form>
-    <div class="sign-txt">Not yet a member? <a href="../Registration/registration.php">Signup now</a></div>
+  <div>
+    <img src="Assets/login.jpg" alt="Login" class="login-image">
   </div>
 
+  <!-- Display error message if login fails -->
+  <?php if (!empty($error_message)): ?>
+    <p class="error"> <?php echo htmlspecialchars($error_message); ?> </p>
+  <?php endif; ?>
+
+  <div>
+    <form action="login.php" method="POST">
+      <label for="email">Email Address</label>
+      <input type="text" id="email" name="email" placeholder="Email Address" required>
+
+      <label for="password">Password</label>
+      <input type="password" id="password" name="password" placeholder="Password" required>
+
+      <label for="user_type">User Type</label>
+      <select id="user_type" name="user_type" required>
+        <option value="">- Select -</option>
+        <option value="administrator">Administrator</option>
+        <option value="student_postgraduate">Student Postgraduate</option>
+        <option value="student_undergraduate">Student Undergraduate</option>
+        <option value="koperasi_staff">Koperasi Staff</option>
+      </select>
+
+      <div class="form-buttons">
+        <button type="submit">Login</button>
+        <button type="button" onclick="window.location.href='signup.php'">Sign Up</button>
+      </div>
+    </form>
+  </div>
+  <br>
+
+  <a href="forgot_password.php">Forgot Password</a>
 </body>
 </html>
